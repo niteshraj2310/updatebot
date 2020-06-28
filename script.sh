3090fba023b5eb112cc2616d251eb43d19269238
Abhinav Shinde <66168862+abhinav6497@users.noreply.github.com>
2020-06-28 11:18:04 +0530

    🌝🌚

--- script.sh
+++ script.sh
@@ -32,7 +32,7 @@ fi
 echo " "
 echo "Updating your UserBot"
 git pull https://github.com/sahyam2019/oub-remix.git
-git commit -m "pehli fursat me ho ja🌝"
+git commit -m "🌝pehli fursat me ho ja🌚"
 git push
 echo " "
 echo "Updated"
