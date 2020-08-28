f814bb6e04264d23fcd5138c13b5d09745f29a91
NiteshRaj <niteshraj231@outlook.com>
2020-08-28 09:15:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ fi
 echo " "
 echo "Updating your UserBot"
 git pull https://github.com/sahyam2019/oub-remix.git
-git commit -m "🌝commit suicide🌚"
+git commit -sm "🌝commit suicide🌚"
 git push
 echo " "
 echo "Updated"
